afef50093ea67fc4343d49929445b234ddbcfd09
Adrian Camilo Sanchez <electrocamo@gmail.com>
2026-08-25 16:33:49 -0500

    cambio de nombre en barra de navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
+        appBar: AppBar(title: const Text('Material App Bar 1')),
         body: const Center(child: Text('Hello World')),
       ),
     );
